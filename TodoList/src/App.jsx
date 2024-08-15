903b9b63ae0b70afaa81cfe914fd2e35fe16b916
Jeonghyun <limithyunii@gmail.com>
2024-08-15 16:04:57 +0900

    Feat: TodoList

---
+++ TodoList/src/App.jsx
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "코딩 테스트 문제 풀기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "CS 과목 공부하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo;
+      })
+    );
+  };
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className='App'>
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List
+        todos={todos}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
+    </div>
+  );
+}
+
+export default App;
